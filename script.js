53530c6db1f1f6bb77bf8631bca104a1280e8e00
Nazir3625m <116695966+Nazir3625m@users.noreply.github.com>
2026-07-14 11:06:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const map = L.map('map').setView([12.8797, 121.7740], 6);
+var map = L.map('map').setView([12.8797, 121.7740], 6);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
+    attribution: '&copy; OpenStreetMap contributors',
+    maxZoom: 19
 }).addTo(map);
