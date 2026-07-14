155e2f617d4d3b2980e8fea4ff092a0a7126534f
Nazir3625m <116695966+Nazir3625m@users.noreply.github.com>
2026-07-14 11:25:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
-var map = L.map('map').setView([12.8797,121.7740],6);
+// Create map
+const map = L.map('map').setView([12.8797, 121.7740], 6);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    attribution:'© OpenStreetMap'
+// Base map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-fetch('data/philippines.geojson')
-.then(res=>res.json())
-.then(data=>{
+// Selected color
+let selectedColor = "#ff0000";
 
-L.geoJSON(data,{
-style:{
-color:"#ffffff",
-weight:1,
-fillColor:"#808080",
-fillOpacity:0.7
-},
-onEachFeature:function(feature,layer){
-
-layer.on('click',function(){
-
-layer.setStyle({
-fillColor:"#ff0000"
-});
-
-console.log(feature.properties);
-
-});
-
-}
+// Temporary demo polygon
+const demo = L.polygon([
+    [14.85,120.82],
+    [14.92,120.98],
+    [14.73,121.03],
+    [14.65,120.87]
+],{
+    color:"#ffffff",
+    weight:2,
+    fillColor:"#808080",
+    fillOpacity:0.7
 }).addTo(map);
 
+demo.on("click",function(){
+    demo.setStyle({
+        fillColor:selectedColor
+    });
 });
