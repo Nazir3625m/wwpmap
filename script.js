db4318763e7240b5781058e4188156bb7cb60f10
Nazir3625m <116695966+Nazir3625m@users.noreply.github.com>
2026-07-14 11:18:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-var map = L.map('map').setView([12.8797, 121.7740], 6);
+var map = L.map('map').setView([12.8797,121.7740],6);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors',
-    maxZoom: 19
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution:'© OpenStreetMap'
 }).addTo(map);
+
+fetch('data/philippines.geojson')
+.then(res=>res.json())
+.then(data=>{
+
+L.geoJSON(data,{
+style:{
+color:"#ffffff",
+weight:1,
+fillColor:"#808080",
+fillOpacity:0.7
+},
+onEachFeature:function(feature,layer){
+
+layer.on('click',function(){
+
+layer.setStyle({
+fillColor:"#ff0000"
+});
+
+console.log(feature.properties);
+
+});
+
+}
+}).addTo(map);
+
+});
